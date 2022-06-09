6be7e0fb51587fa9ac847a0936eafa18bf864dff
Andrey <47784042+Beliyandr@users.noreply.github.com>
2022-06-09 10:46:31 +0300

    l5

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+
+import "./scss/app.scss";
+
+import { Header } from "./components/Header/Header";
+import { Categories } from "./components/Categories/Categories";
+import { Sort } from "./components/Sort/Sort";
+import { PizzaBlock } from "./components/PizzaBlock/PizzaBlock";
+
+import pizzas from "./assets/pizzas.json";
+
+console.log(pizzas);
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            {/* <Categories /> */}
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
